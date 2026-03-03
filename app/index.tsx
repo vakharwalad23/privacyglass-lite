fd60afa7761d285c1efdb3df48e59356529fe893
vakharwalad23 <vakharwala.d@gmail.com>
2026-03-03 23:33:12 +0530

    fix: initial cleanup

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HelloScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+});
